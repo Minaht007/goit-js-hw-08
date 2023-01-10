55e948df8b6a26521306abd3897f335ad899d099
Andrey <49126678+Minaht@users.noreply.github.com>
2023-01-10 11:25:57 +0200

    first gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </div>`
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
