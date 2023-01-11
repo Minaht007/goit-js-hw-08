2ab1ecefbb96e87f7dd615dc9061756f41277ed4
Andrey <49126678+Minaht@users.noreply.github.com>
2023-01-11 14:26:57 +0200

    checkplaeyr

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,53 +2,18 @@ import Player from '@vimeo/player';
 
 // console.log(Player);
 
-const vimeoPlayer = document.querySelector('#vimeo-player');
 
-const onPlay = function(data) {
-    const play = vimeoPlayer.addEventListener(click, onAddLocal)
-};
 
-//  function onAddLocal(onPlay) {
-//     onPlay.set
-//  }
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+checkPlayTimeProgress();
+player.play().then(function() {
+    
 
-vimeoPlayer.on('play', onPlay);
+player.on(timeupdate, onPlay);
+})
 
-{/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" 
-width="640" height="360" 
-frameborder="0" 
-allowfullscreen allow="autoplay; 
-encrypted-media"></iframe>
-
-<script src="https://player.vimeo.com/api/player.js"></script>
-<script>
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-</script> */}
-
-// player.pause().then(function() {
-//     // the video was paused
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'PasswordError':
-//             // the video is password-protected and the viewer needs to enter the
-//             // password first
-//             break;
-
-//         case 'PrivacyError':
-//             // the video is private
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
+function checkPlayTimeProgress() {
+    localStorage.getItem('playProgress')
+    if(progress)player.setCurrentTime(progress);
+}
