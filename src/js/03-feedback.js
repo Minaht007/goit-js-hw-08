d470d80185e7d47a3a872b24bdb52fd4060cb7fc
Andrey <49126678+Minaht@users.noreply.github.com>
2023-01-12 15:10:06 +0200

    third possition

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'selectedFilters';
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+let formData = {};
+
+onPageLoad();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  console.dir(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  formData = {};
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onPageLoad() {
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedData) {
+    Object.entries(savedData).forEach(([key, value]) => {
+      formEl[key].value = value;
+      formData[key] = value;
+    });
+  }
+}
